073f381309f8165c1214a0c60aad69eebfd055e3
Matt Dean <matt@trabian.com>
2010-11-11 17:05:54 -0500

    Hackish zendesk ticket queue

--- plugins/zendesk/zendesk.plugin.zsh
+++ plugins/zendesk/zendesk.plugin.zsh
@@ -12,5 +12,35 @@ open_zendesk_ticket () {
   fi
 }
 
+push_ticket() {
+  `echo $1 >> ~/.zendesk-tickets`
+  update_motd
+}
+
+update_motd() {
+  `echo "Here's some tickets you may want to work on: " > /etc/motd`
+  `cat ~/.zendesk-tickets >> /etc/motd`
+}
+
+peek_ticket() {
+
+  ticket=$(head -1 ~/.zendesk-tickets)
+  zendesk_url=$(cat ~/.zendesk-url);
+
+  `open $zendesk_url/tickets/$ticket`
+
+}
+
+pop_ticket() {
+  peek_ticket
+  `sed 1d ~/.zendesk-tickets > ~/.zendesk-tickets-tmp`
+  `mv ~/.zendesk-tickets{-tmp,}`
+  update_motd
+}
+
 alias zo='open_zendesk_ticket'
 alias ticket='open_zendesk_ticket'
+
+alias pusht='push_ticket'
+alias peekt='peek_ticket'
+alias popt='pop_ticket'
